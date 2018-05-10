11f5a897cd4cf8cd177617d2bebf509ddacc1841
JLRC <tangun73@gmail.com>
2018-05-10 13:10:48 +0200

    added celery and some dummy tasks

--- api/views.py
+++ api/views.py
@@ -2,8 +2,14 @@ from rest_framework import viewsets
 
 from api.models import Test
 from api.serializers import TestSerializer
+from .task import mul
 
 
 class TestViewSet(viewsets.ModelViewSet):
     queryset = Test.objects.all()
     serializer_class = TestSerializer
+
+    def create(self, request, *args, **kwargs):
+        ret = super(TestViewSet, self).create(request, *args, **kwargs)
+        mul.delay(2, 2)
+        return ret
