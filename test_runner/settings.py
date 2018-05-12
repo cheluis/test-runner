3b5ecb36ae09c663a7b7ad5014b82c10f0bb0b04
JLRC <tangun73@gmail.com>
2018-05-12 14:12:25 +0200

    addec cors headers

--- test_runner/settings.py
+++ test_runner/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'api',
 ]
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +123,5 @@ CELERY_IMPORTS = (
 )
 
 VIRTUAL_ENV_PATH = env('VIRTUAL_ENV_PATH')
+
+CORS_ORIGIN_ALLOW_ALL = True
