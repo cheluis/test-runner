b18bceee1111a017b36a4ab0f7e23bcf3026d969
JLRC <tangun73@gmail.com>
2018-05-14 23:17:13 +0200

    added log to task, added another test, added queryset ordering

--- api/task.py
+++ api/task.py
@@ -2,15 +2,19 @@
 from __future__ import absolute_import, unicode_literals
 import subprocess
 
+from celery.utils.log import get_task_logger
+
+
 from django.conf import settings
 from celery import shared_task
 
 from api.models import Test
 ENV = settings.VIRTUAL_ENV_PATH
 
-
+logger = get_task_logger(__name__)
 @shared_task()
 def run_script(test_id):
+    logger.info("En la tarea")
     instance = Test.objects.filter(id=test_id).first()
     cmd = "pytest {}".format(instance.get_script())
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,
@@ -21,5 +25,7 @@ def run_script(test_id):
         instance.status = "F"
     else:
         instance.status = "P"
+
+    logger.info("En la tarea: {}".format(out_str))
     instance.log_information = out_str
     instance.save()
---
+++ test_runner/tests/test_3.py
@@ -0,0 +1,8 @@
+def mult(x, y):
+    return x * y
+
+def test_answer_1():
+    assert mult(3, 2) == 6
+
+def test_answer_2():
+    assert mult(2, 6) == 5
