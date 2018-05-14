31aa8470904ee8b5a7194590ef353efce8964a7a
JLRC <tangun73@gmail.com>
2018-05-15 00:15:18 +0200

    added validation for tests enviroment

--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,17 @@ from .models import Test
 class TestSerializer(serializers.ModelSerializer):
     script_tested = serializers.ReadOnlyField()
 
+    def validate(self, attrs):
+        try:
+            Test.objects.get(test_enviroment_id=attrs['test_enviroment_id'],
+                             status="C")
+        except Test.DoesNotExist:
+            pass
+        else:
+            raise serializers.ValidationError('Test running on given environment')
+
+        return attrs
+
     class Meta:
         model = Test
         fields = ('id', 'username', 'test_enviroment_id', 'test_template',
